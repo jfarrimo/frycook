0c1ff0b8a05544096b337d8eae2562d55e33450d
Jay Farrimond <jay@instaedu.com>
2012-10-19 20:01:34 -0700

    bumped to v0.1.4

--- src/setup.py
+++ src/setup.py
@@ -28,7 +28,7 @@
 from distutils.core import setup
 
 setup(name='frycook',
-      version='0.1.3',
+      version='0.1.4',
       description='frycook system builder',
       author='Jay Farrimond',
       author_email='jay@farrimond.com',
