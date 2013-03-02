96bc8d00f1939edfa8741edba3c077a53ea3e744
Jay Farrimond <jay@instaedu.com>
2013-03-01 16:53:28 -0800

    was calling wrong message function name on recipes from frycooker

--- src/setup.py
+++ src/setup.py
@@ -28,7 +28,7 @@
 from distutils.core import setup
 
 setup(name='frycook',
-      version='0.2.14',
+      version='0.2.15',
       description='frycook system builder',
       author='Jay Farrimond',
       author_email='jay@farrimond.com',
